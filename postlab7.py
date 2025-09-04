a256cbdb94823c9c88ded100106caa40318a1e7a
Kaavya Rao <124367753+KaavyaRao09@users.noreply.github.com>
2025-09-04 21:46:35 +0530

    Add files via upload

---
+++ postlab7.py
@@ -0,0 +1,40 @@
+# import numpy as np
+# a = np.arange(2, 11).reshape(3, 3)
+# print(a)
+
+
+# import numpy as np
+# a = np.arange(2, 11).reshape(3, 3)
+# reversed = a[::-1, ::-1]
+# print(a)
+# print(reversed)
+
+
+# import numpy as np
+# a1 = np.array([1, 2, 3, 4, 5])
+# a2 = np.array([4, 5, 6, 7, 8])
+# a3 = np.intersect1d(a1, a2)
+# print(a3)
+
+
+# import numpy as np
+# a = np.array([1, 2, 3])
+# a2 = np.repeat(a, 3)
+# print(a2)
+
+
+# import numpy as np
+# a = np.array([1, 2, 3, 4])
+# print(a.nbytes, "bytes")
+
+
+# import numpy as np
+# a1 = np.zeros((3, 3))
+# a2 = np.ones((3, 3))
+# print("Array of zeros:\n", a1)
+# print("Array of ones:\n", a2)
+
+
+import numpy as np
+a = np.array([10, 20, 30, 40, 50])
+print(a[3])
