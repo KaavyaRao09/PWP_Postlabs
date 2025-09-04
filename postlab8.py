41c7a63d626e60912181fbb344ffd751d824ea0a
Kaavya Rao <124367753+KaavyaRao09@users.noreply.github.com>
2025-09-04 22:12:01 +0530

    Add files via upload

---
+++ postlab8.py
@@ -0,0 +1,16 @@
+# import math
+# degree = 90
+# radian = math.radians(degree)
+# print(radian)
+
+
+# import math
+# x = 2
+# y = 6 * x**2 + 4 * math.sin(x)
+# print(y)
+
+
+import math
+x = math.pi
+print(math.cos(2*x), -2*math.sin(2*x), -4*math.cos(2*x))
+
