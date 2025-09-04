e8b5fecc60b2cb81f74bc3d2593ad4613112bcba
Kaavya Rao <124367753+KaavyaRao09@users.noreply.github.com>
2025-09-04 21:25:30 +0530

    Add files via upload

---
+++ postlab5.py
@@ -0,0 +1,36 @@
+# a = (1, 2, 2, 3, 4, 2)
+# print(a.count(2))
+
+
+# a = (1, 2, 3, 4)
+# print(3 in a)
+
+
+# t = ('K', 'A', 'A', 'V', 'Y', 'A')
+# result = ''.join(t)
+# print("Tuple:", t)
+# print("String:", result)
+
+
+# t = (5, 10, 3, 8, 2)
+# max = max(t)
+# min = min(t)
+# print(f"Maximum element:", max)
+# print(f"Minimum element:", min)
+
+
+# a = ("Hello", "world", "from", "Python")
+# string = " ".join(a)
+# print(string)
+
+
+# a = (5, 3, 8, 1, 2)
+# sorted_tuple = tuple(sorted(a))
+# print(sorted_tuple)
+
+
+a = (5, 3, 8, 1, 2)
+first = a[0]
+last = a[-1]
+print("First element:", first)
+print("Last element:", last)
